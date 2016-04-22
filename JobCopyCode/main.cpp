d7f40e72aab928a4c531a1d4c548ae6b831c8759
wliuxingxiangyu <wliuxingxiangyu@163.com>
2016-04-22 18:29:36 +0800

    printf("\n %s \n",c[i]);//c[i]指向的是字符串,输出abcd ef gh ijk.

--- JobCopyCode/main.cpp
+++ JobCopyCode/main.cpp
@@ -3,14 +3,15 @@
 #include <cstdio>
 #include <algorithm>
 #include <stack>
+#include <vector>
 using namespace std;
 void testCharArr(){
 	char* c[]={"abcd","ef","gh","ijk"};
 	for (int i=0;i<4;i++){ 
 		printf("%c",*c[i]);//输出每个字符串的首字符aegi
 		// printf("%s",*c[i]);//*c[i]指向的是字符,不能按%s字符串输出.
+		printf("\n %s \n",c[i]);//c[i]指向的是字符串,输出abcd ef gh ijk.
 	}
-
 }
 int main(){
 	testCharArr();
